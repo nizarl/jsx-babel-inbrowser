3f2a2206e044a5f7285ec93e75515edc000e2b90
Nizar Lakhdhir <nizarl@chenmed.com>
2017-07-16 20:19:18 -0400

    prototype umd in browser

---
+++ main.js
@@ -0,0 +1,12 @@
+class Example extends React.Component {
+  render() {
+    return (
+      <MyComponent />
+      
+    );
+  }
+}
+
+
+ReactDOM.render(<Example />,document.getElementById('app')
+);
